6c7fa299c5e5303e846de2114ddc5fa1c1138f5e
Przemysław Pieczara <przemekpieczara@op.pl>
2025-02-06 20:31:22 +0100

    Creating a helper for the sky state

--- app/helpers/weather_helper.rb
+++ app/helpers/weather_helper.rb
@@ -1,2 +1,11 @@
 module WeatherHelper
+  def weather_emoji(station)
+    if station["suma_opadu"].to_f > 0.0
+      "🌧️"
+    elsif station["temperatura"].to_f > 20
+      "☀️"    
+    else
+      "☁️"
+    end
+  end
 end
