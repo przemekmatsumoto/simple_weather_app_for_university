e7247223db2a2466fa6fd05021af418dd1bc504b
Przemysław Pieczara <przemekpieczara@op.pl>
2025-02-06 20:29:55 +0100

    Add logic and change views

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,4 +1,16 @@
 class WeatherController < ApplicationController
   def index
+    if params[:miasto].present?
+      all_data = ImgwService.new.fetch_synop_data
+      if all_data
+        @dane_pogodowe = all_data.select do |stacja|
+          stacja["stacja"].downcase.include?(params[:miasto].strip.downcase)
+        end
+      else
+        @dane_pogodowe = []
+      end
+    else
+      @dane_pogodowe = []
+    end
   end
 end
